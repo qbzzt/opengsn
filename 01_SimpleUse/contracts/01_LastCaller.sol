4f931022bc27f3802a9b2410a31bb5b47ea5b405
qbzzt <qbzzt1@gmail.com>
2020-04-27 17:13:32 -0500

    Update 01_LastCaller.sol

--- 01_SimpleUse/contracts/01_LastCaller.sol
+++ 01_SimpleUse/contracts/01_LastCaller.sol
@@ -9,6 +9,8 @@ contract LastCaller is BaseRelayRecipient {
 
 	address lastCaller = address(0);
 
+	// To use the GSNv2 forwarder on Kovan, call with
+	// 0x6453d37248ab2c16ebd1a8f782a2cbc65860e60b
 	constructor(address _forwarder) public {
 		trustedForwarder = _forwarder;
 	}
