9430346ef7f5b8547b5a29d38bdf24b8bae4dd65
Ori Pomerantz <qbzzt1@gmail.com>
2020-05-12 20:58:39 +0000

    truffle test now tests through gsn

--- 01_SimpleUse/test/testcontracts.js
+++ 01_SimpleUse/test/testcontracts.js
@@ -1,13 +1,36 @@
-const gsn = require('@opengsn/gsn')
-const gsnHelpers = require('@opengsn/gsn/helpers')
-const Web3 = require('web3')
+const blockchain = "http://chain:8545"
 
-const LastCaller = artifacts.require('../contracts/LastCaller.sol')
+const gsn = require('@opengsn/gsn')
+
+const RelayProvider = require("@opengsn/gsn/dist/src/relayclient/").RelayProvider
+
+const gsnTestEnv = require('@opengsn/gsn/dist/GsnTestEnvironment').default
+const configureGSN = require('@opengsn/gsn/dist/src/relayclient/GSNConfigurator').configureGSN
+
+
+const Web3 = require('web3')
+const ethers = require("ethers")
+const LastCaller = artifacts.require('LastCaller')
 const NaivePaymaster = artifacts.require('NaivePaymaster')
+
+
+const callThroughGsn = async (contract, provider) => {
+		const transaction = await contract.getLastCaller()
+		const receipt = await provider.waitForTransaction(transaction.hash)
+
+		const result = receipt.logs.
+			map(entry => contract.interface.parseLog(entry)).
+			filter(entry => entry != null)[0];
+
+		return result.values['0']
+
+};  // callThroughGsn
+
+
 
 contract("LastCaller", async accounts => {
 
-	it ('text clause text', async () => {
+	it ('Runs without GSN', async () => {
 		const lastcaller = await LastCaller.new('0x0000000000000000000000000000000000000000');
 
 		const res = await lastcaller.getLastCaller();
@@ -23,73 +46,66 @@ contract("LastCaller", async accounts => {
 		assert.equal(res3.logs[0].args["0"], res2.logs[0].args["0"],
 			"Wrong third last caller");
 
-	});
+	});   // it 'Runs without GSN'
+
+	it ('Runs with GSN', async () => {
+		const gsnInstance = await gsnTestEnv.startGsn(blockchain);
+
+		const lastCaller = await 
+			LastCaller.new(gsnInstance.deploymentResult.forwarderAddress)
+
+		const paymaster = await NaivePaymaster.new()
+		await paymaster.setRelayHub(gsnInstance.deploymentResult.relayHubAddress)
+		await paymaster.send(1e17)
+		await paymaster.setTarget(lastCaller.address)
 
 
+		const gsnConfigParams = {
+			gasPriceFactorPercent: 70,
+			methodSuffix: '_v4',
+			jsonStringifyRequest: true,
+			chainId: '*',
+			relayLookupWindowBlocks: 1e5,
+			preferredRelays: [ gsnInstance.relayUrl ],
+			relayHubAddress: gsnInstance.deploymentResult.relayHubAddress,
+			stakeManagerAddress: gsnInstance.deploymentResult.stakeManagerAddress,
+			paymasterAddress: paymaster.address
+		}
 
-  describe('same with GSNDevProvider', function () {
-    let gaslessAccount
-    let lastcaller
-    let transactionDetails
+		const gsnConfig = configureGSN(gsnConfigParams)
 
-    before(async function () {
-      // GsnDevProvider cannot use Truffle's default 'web3.currentProvider', which is 'HttpProvider'
-      // (because HttpProvider does not allow 'event subscriptions')
-      const wssProvider = new Web3.providers.WebsocketProvider(web3.currentProvider.host)
-      const {
-        relayHubAddress,
-        stakeManagerAddress,
-        forwarderAddress
-      } = await gsnHelpers.deployRelayHub(web3, { workdir: 'build/gsn' })
-      // GsnDevProvider requires some configuration (should be simplified in the near future)
-      const devConfig = {
-        relayOwner: accounts[0],
-        relayHubAddress,
-        stakeManagerAddress,
-        gasPriceFactor: 1,
-        pctRelayFee: 0,
-        baseRelayFee: 0
-      }
-      const gsnDevProvider = new gsn.GsnDevProvider(wssProvider, devConfig)
+		const provider = new ethers.providers.Web3Provider(
+			new RelayProvider(web3.currentProvider, gsnConfig)) 
 
-      // recipient must know what is the forwarder address
-      lastcaller = await LastCaller.new(forwarderAddress)
 
-      // paymaster must know both relay hub and target
-      const naivePaymaster = await NaivePaymaster.new()
-      await naivePaymaster.setRelayHub(relayHubAddress)
-      await naivePaymaster.setTarget(lastcaller.address)
+		const acct = provider.provider.newAccount()
+		const contract = await new ethers.Contract(lastCaller.address, lastCaller.abi,
+			provider.getSigner(acct.address, acct.privateKey))
 
-      // Paymaster will forward all incoming ether to the RelayHub
-      // There is no 'balance' of the paymaster contract itself, only it's record in the Hub.
-      await web3.eth.sendTransaction({
-        from: accounts[0],
-        to: naivePaymaster.address,
-        value: 1e18
-      })
+		var result = await callThroughGsn(contract, provider);
+		assert.equal(result, 0, "Wrong initial last caller");
 
-      // Now, this line is magic. We know how does Truffle implements sending transactions internally, so we set the provider through one of the 'artifacts'
-      // Outside the scope of test framework this will be done in the opposite order
-      LastCaller.web3.setProvider(gsnDevProvider)
-      gaslessAccount = gsnDevProvider.newAccount()
+		var result = await callThroughGsn(contract, provider);
+		assert.equal(result.toLowerCase(), acct.address.toLowerCase(), 
+			"Wrong second last caller (should be acct)");
 
-      // The last parameter of the contract call is 'details'. GSN calls have some extra fields that must be specified.
-      transactionDetails = {
-        from: gaslessAccount.address,
-        paymaster: naivePaymaster.address,
-        forwarder: forwarderAddress // This parameter is still debated. I will include it, but as of today, this is not the source of this information. 'forwarderAddress' today is read from the recipient
-      }
-    })
 
-    // But after the long setup, the usage is exactly as it was before the GSN.
-    it('should emit user address', async function () {
-      await lastcaller.getLastCaller(transactionDetails)
-      const res = await lastcaller.getLastCaller(transactionDetails)
-      assert.equal(res.logs[0].event, 'LastCallerIs', 'Wrong event')
-      assert.equal(res.logs[0].args['0'].toLowerCase(), gaslessAccount.address.toLowerCase(), 'Wrong last caller')
-    })
-  })
+		const acct2 = provider.provider.newAccount()
+		const contract2 = await new ethers.Contract(lastCaller.address, lastCaller.abi,
+			provider.getSigner(acct2.address, acct2.privateKey))
 
-  
-})
+
+		var result = await callThroughGsn(contract2, provider);
+		assert.equal(result.toLowerCase(), acct.address.toLowerCase(), 
+			"Wrong third last caller (should be acct)");		
+
+
+		var result = await callThroughGsn(contract, provider);
+		assert.equal(result.toLowerCase(), acct2.address.toLowerCase(),
+			"Wrong fourth last caller (should be acct2)");
+
+
+	});   // it 'Runs with GSN'
+
+});   // describe
 
