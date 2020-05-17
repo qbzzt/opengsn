3ac9d6235b341f70b472d466254885d7ab964994
Alex Forshtat <forshtat1@gmail.com>
2020-05-17 23:56:46 +0300

    Use truffle migrations to set forwarder

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,4 +2,9 @@ const CaptureTheFlag = artifacts.require('CaptureTheFlag')
 
 module.exports = async function (deployer) {
   await deployer.deploy(CaptureTheFlag)
+
+  const instance = await CaptureTheFlag.deployed()
+  const forwarderAddress = require('../build/gsn/Forwarder.json').address
+  await instance.setTrustedForwarder(forwarderAddress)
+  console.log(`Successfully set Trusted Forwarder (${forwarderAddress}) on Recipient (${instance.address})`)
 }
