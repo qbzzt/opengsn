4a60ef6602e95d960263cbe421f8fe04909cc3ec
Ori Pomerantz <qbzzt1@gmail.com>
2020-12-20 01:15:59 +0000

    Updated for 2.0.3

--- 01_SimpleUse/ui/etherless.js
+++ 01_SimpleUse/ui/etherless.js
@@ -2,59 +2,37 @@ var Web3 = require( 'web3')
 const gsn = require('@opengsn/gsn')
 const ethers = require("ethers")
 
-//TODO: temporary struct, to run from command-line
 
-if (typeof window == 'undefined' ) {
-  function alert() {
-    console.log( 'alert:',...Array.prototype.slice.call(arguments))
-  }
-
-  window = {
-    ethereum:new Web3.providers.HttpProvider( 'https://kovan.infura.io/v3/f40be2b1a3914db682491dc62a19ad43')
-  }
-  window.ethereum.enable=()=>1
-}
-
-//TODO: temporary fix (gsn references global web3.eth.abi)
-global.web3 = new Web3(window.ethereum)
 
 const conf = {
-	ourContract: '0x9576f163350b33Bb75CFB5A4E6B123E5c2AbdaD8',
+	ourContract: '0x10A51A94d096e746E1Aec1027A0F8deCEC43FF63',
 	notOurs:     '0x6969Bc71C8f631f6ECE03CE16FdaBE51ae4d66B1',
-	paymaster:   '0x9940c8e12Ca14Fe4f82646D6d00030f4fC3C7ad1',
-	relayhub:    '0xcfcb6017e8ac4a063504b9d31b4AbD618565a276',
-        forwarder:   '0x663946D7Ea17FEd07BF1420559F9FB73d85B5B03',
+	paymaster:   '0x3f84367c25dC11A7aBE4B9ef97AB78d5D5498bF5',
+	relayhub:    '0xE9dcD2CccEcD77a92BA48933cb626e04214Edb92',
+        forwarder:   '0x0842Ad6B8cb64364761C7c170D0002CC56b1c498',
 	gasPrice:  20000000000   // 20 Gwei
 }
 
 
-var provider = false
+var provider
 var userAddr   // The user's address
 
 
 const startGsn = async () => {
-	if (provider)
-		return;
-
 	await window.ethereum.enable()
 
-	const gsnConfig =
-		await gsn.resolveConfigurationGSN(window.ethereum, {
-//			verbose: true,
-			chainId: window.ethereum.chainId,
-			paymasterAddress: conf.paymaster,
-			forwarderAddress: conf.forwarder,
-			methodSuffix: '_v4',
-  			jsonStringifyRequest: true
-		})
-	window.app.gsnConfig = gsnConfig
+	if (provider != undefined)
+		return;
 
-	const gsnProvider = new gsn.RelayProvider(window.ethereum, gsnConfig)
-        window.app.gsnProvider = gsnProvider
 
+        let gsnProvider = await new gsn.RelayProvider(window.ethereum, {
+		forwarderAddress: conf.forwarder,
+            	paymasterAddress: conf.paymaster,
+                verbose: false}).init()
 	provider = new ethers.providers.Web3Provider(gsnProvider)
-//	userAddr = gsnProvider.newAccount().address
 	userAddr = gsnProvider.origProvider.selectedAddress
+
+	window.app.gsnProvider = gsnProvider
 	window.app.provider = provider
 	window.app.userAddr = userAddr
 }
@@ -210,12 +188,14 @@ const listenToEvents = async () => {
 	const contract = await new ethers.Contract(
 		conf.ourContract, data.abi, provider);
 
-	contract.on(contract.interface.events.FlagCaptured,
-		evt => console.log(`Event: ${
-			JSON.stringify(contract.interface.parseLog(evt))}`)
-	);
+        window.app.listenContract = contract
 
-};  // listenToEvents
+	contract.on("FlagCaptured",
+		(captureFrom, captureTo, evt) =>
+			alert(`Capture: ${captureFrom} -> ${captureTo}`)
+	)
+
+}  // listenToEvents
 
 
 window.app = {
